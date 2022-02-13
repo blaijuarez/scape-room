7ec345bcb93d88a693c3d58b7320ddfb69deb9a5
Blai Juárez Abellán <blaijuarez@gmail.com>
2022-02-13 15:36:39 +0100

    fase final

---
+++ fase-final/script.js
@@ -0,0 +1,605 @@
+var IMAGENES = [
+  {
+    path: "1.png",
+    text1: "Titulo para la foto 1",
+    text2: "Texto para la foto 1",
+  },
+  {
+    path: "2.png",
+    text1: "Titulo para la foto 2",
+    text2: "Texto para la foto 2",
+  },
+  {
+    path: "3.png",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "4.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "5.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "6.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "7.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "8.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "9.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "10.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "11.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "12.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "13.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "14.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "15.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "16.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "17.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "18.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "19.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "20.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "21.jpeg",
+    text1: "Titulo para la foto 1",
+    text2: "Texto para la foto 1",
+  },
+  {
+    path: "22.jpeg",
+    text1: "Titulo para la foto 2",
+    text2: "Texto para la foto 2",
+  },
+  {
+    path: "23.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "24.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "25.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "26.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "27.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "28.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "29.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "30.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "31.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "32.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "33.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "34.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "35.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "36.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "37.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "38.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "39.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "40.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "41.jpeg",
+    text1: "Titulo para la foto 1",
+    text2: "Texto para la foto 1",
+  },
+  {
+    path: "42.jpeg",
+    text1: "Titulo para la foto 2",
+    text2: "Texto para la foto 2",
+  },
+  {
+    path: "43.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "44.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "45.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "46.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "47.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "48.png",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "49.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "50.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "51.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "52.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "53.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "54.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "55.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "56.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "57.png",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "58.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "59.png",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "60.png",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "61.jpeg",
+    text1: "Titulo para la foto 1",
+    text2: "Texto para la foto 1",
+  },
+  {
+    path: "62.jpeg",
+    text1: "Titulo para la foto 2",
+    text2: "Texto para la foto 2",
+  },
+  {
+    path: "63.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "64.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "65.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "66.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "67.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "68.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "69.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "70.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "71.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "72.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "73.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "74.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "75.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "76.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "77.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "78.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "79.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "80.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "81.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "82.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "83.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "84.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "85.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "86.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "87.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "88.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "89.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "90.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "91.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+  {
+    path: "92.jpeg",
+    text1: "Titulo para la foto 3",
+    text2: "Texto para la foto 3",
+  },
+];
+
+!(function ($) {
+  var defaults = {
+    sectionContainer: "> section",
+    angle: 50,
+    opacity: true,
+    scale: true,
+    outAnimation: true,
+    pageContainer: ".page_container",
+    pageOpacity: true,
+  };
+  $.fn.tiltedpage_scroll = function (options) {
+    var settings = $.extend({}, defaults, options),
+      el = $(this);
+    el.find(settings.sectionContainer).addClass("tps-section");
+    el.find(".tps-section").each(function (index) {
+      var el2 = $(this);
+      el2.wrapInner("<div class='tps-wrapper'></div>");
+      var addBg = el2.find(".tps-wrapper");
+      debugger;
+      addBg.css({
+        background: `url(photos/${IMAGENES[index].path}) center center`,
+        "background-size": "cover",
+      });
+    });
+
+    function isElementInViewport(el3) {
+      var docViewTop = $(window).scrollTop(),
+        docViewBottom = docViewTop + $(window).height(),
+        elemTop = el3.offset().top,
+        elemBottom = elemTop + el3.outerHeight(true);
+      return elemBottom >= docViewTop && elemTop <= docViewBottom;
+    }
+
+    function elementVisibilityMayChange(el4) {
+      if (isElementInViewport(el4)) {
+        el4.addClass("tps-inview");
+      } else {
+        el4.removeClass("tps-inview");
+      }
+    }
+    $(window).on("DOMContentLoaded load resize scroll", function () {
+      el.find(settings.sectionContainer).each(function () {
+        elementVisibilityMayChange($(this));
+      });
+      el.find(".tps-section.tps-inview > .tps-wrapper").each(function (index) {
+        var el2 = $(this),
+          elc = el2.find(settings.pageContainer),
+          opacity = 0,
+          opacity2 = 0,
+          st = $(window).scrollTop(),
+          deg =
+            ((el2.parent().offset().top - el2.parent().height() - st) /
+              $(window).height()) *
+            (settings.angle * 3),
+          scale =
+            (st +
+              $(window).height() -
+              (el2.parent().offset().top - el2.parent().height())) /
+            $(window).height();
+        if (scale > 1) scale = 1;
+        if (deg < 0) deg = 0;
+        if (st > el2.parent().offset().top) {
+          if (settings.outAnimation == false) {
+            opacity = 1;
+            opacity2 = 1;
+            if (opacity < 0) {
+              opacity = 0;
+              opacity2 = 0;
+            }
+            if (deg < 0) deg = 0;
+          } else {
+            opacity =
+              (el2.parent().offset().top + $(window).height() * 1.2 - st) /
+              $(window).height();
+            opacity2 = opacity;
+            opacity = Math.pow(opacity, 25);
+            opacity2 = Math.pow(opacity2, 25);
+            //console.log('- '+opacity2);
+            deg =
+              ((el2.parent().offset().top - st) / $(window).height()) *
+              (settings.angle * 3);
+            scale =
+              (st + $(window).height() - el2.parent().offset().top) /
+              $(window).height();
+          }
+        } else {
+          if (index != 0) {
+            opacity =
+              (st +
+                $(window).height() -
+                el2.parent().offset().top +
+                el2.height() / 2) /
+              $(window).height();
+            opacity2 = opacity / 2;
+            opacity2 = opacity2 < 0.4 ? opacity2 / 2 : opacity2;
+            //console.log(opacity2);
+            if (opacity > 1) {
+              opacity = 1;
+              opacity2 = 1;
+            }
+          } else {
+            opacity = 1;
+            opacity2 = 1;
+            deg = 0;
+            scale = 1;
+          }
+        }
+        if (settings.scale == false) scale = 1;
+        if (settings.angle == false) deg = 0;
+        if (settings.opacity == false) {
+          opacity = 1;
+          opacity2 = 1;
+        }
+        el2.css({
+          transform:
+            "rotateX(" + deg + "deg) scale(" + scale + ", " + scale + ")",
+          opacity: opacity,
+        });
+        elc.css({ opacity: opacity2 });
+      });
+    });
+  };
+})(window.jQuery);
+
+$(document).ready(function () {
+  _pintarImagenes();
+
+  $(".main").tiltedpage_scroll({
+    angle: 20,
+  });
+});
+
+function _pintarImagenes() {
+  var container = document.querySelector(".main");
+  var nodo = document.querySelector("section");
+
+  IMAGENES.forEach(({ text1, text2 }) => {
+    var clon = nodo.cloneNode();
+    clon.innerHTML = `<h1><a href="#">${text1}<br /><small>${text2}</small></a></h1>`;
+    container.appendChild(clon);
+  });
+}
